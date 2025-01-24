6518516cf2a95559934c4ea76b5b1e9cb2a6f7a5
Fi44er <vaceslavmalugin81@gmail.com>
2025-01-24 10:42:28 +0300

    sprint-1

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fitnessapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
         setContent {
             val navController = rememberNavController()
             FitnessAppTheme(
