7edbc16073261c32092d7c7759fb444ea4f66c8e
Fi44er <vaceslavmalugin81@gmail.com>
2025-01-25 11:01:16 +0300

    RED

--- app/src/androidTest/java/com/example/fitnessapp/QueueTest.kt
+++ app/src/androidTest/java/com/example/fitnessapp/QueueTest.kt
@@ -1,6 +1,5 @@
 package com.example.fitnessapp
 
-import android.util.Log
 import android.util.Patterns
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Column
@@ -10,26 +9,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
-import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
-import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.fitnessapp.feature_app.domain.usecase.Queue.CheckQueueUseCase
-import com.example.fitnessapp.feature_app.domain.usecase.Queue.ClearQueueUseCase
-import com.example.fitnessapp.feature_app.domain.usecase.Queue.GetQueueUseCase
-import com.example.fitnessapp.feature_app.domain.usecase.Queue.QueueUseCase
-import com.example.fitnessapp.feature_app.domain.usecase.Queue.SetQueueUseCase
-import com.example.fitnessapp.feature_app.presentation.OnBoard.OnBoardItem
-import com.example.fitnessapp.feature_app.presentation.OnBoard.OnBoardScreen
-import com.example.fitnessapp.feature_app.presentation.OnBoard.OnBoardViewModel
-import com.example.fitnessapp.feature_app.presentation.OnBoard.onBoardItemList
-import kotlinx.coroutines.runBlocking
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -37,46 +22,25 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class QueueTest {
 
-    private lateinit var viewModel: OnBoardViewModel
-    private lateinit var queueUseCase: QueueUseCase
-
     @get:Rule
     val rule = createAndroidComposeRule<ComponentActivity>()
 
-    @Before
-    fun clientInit(){
-        val manger = QueueMangerImpl()
-
-        queueUseCase = QueueUseCase(
-            GetQueueUseCase(manger),
-            SetQueueUseCase(manger),
-            ClearQueueUseCase(manger),
-            CheckQueueUseCase(manger)
-        )
-
-        viewModel = OnBoardViewModel(
-            queueUseCase
-        )
-
-
-    }
-
     @Test
-    fun u(){
+    fun emailValidateFailed(){
         rule.setContent {
-            val t = remember { mutableStateOf("") }
            Column {
+               val text = remember { mutableStateOf("") }
                TextField(
-                   value = t.value,
+                   value = text.value,
                    onValueChange = {
-                       t.value = it
+                       text.value = it
                    },
                    modifier = Modifier
-                       .testTag("tf")
+                       .testTag("email")
                )
                Button(
                    onClick = {
-
+                        assert(Patterns.EMAIL_ADDRESS.matcher(text.value).matches())
                    },
                    modifier = Modifier
                        .testTag("btn")
@@ -84,52 +48,10 @@ class QueueTest {
            }
         }
 
-        rule.onAllNodesWithTag("tf").onFirst().performTextInput("qwerty")
-        rule.onAllNodesWithTag("tf").onFirst().assertTextEquals("qwerty")
+        rule.onAllNodesWithTag("email")
+            .onFirst().performTextInput("qwerty")
+        rule.onAllNodesWithTag("btn").onFirst().performClick()
     }
 
-    @Test
-    fun firstTest(){
-        runBlocking {
-            queueUseCase.clearQueueUseCase()
 
-            onBoardItemList = listOf(
-                OnBoardItem(
-                    R.drawable.onboard_screen1,
-                    "test1",
-                    "description1"
-                ),
-                OnBoardItem(
-                    R.drawable.onboard_screen2,
-                    "test2",
-                    "description2"
-                ),
-                OnBoardItem(
-                    R.drawable.onboard_screen2,
-                    "test3",
-                    "description3"
-                ),
-            )
-            Log.e("m", onBoardItemList.toString())
-
-            viewModel = OnBoardViewModel(
-                queueUseCase
-            )
-
-        }
-
-        rule.setContent {
-            OnBoardScreen(
-                navController = rememberNavController(),
-                viewModel = viewModel
-            )
-        }
-
-        rule.onAllNodesWithText("test1").onFirst().assertExists()
-        rule.onAllNodesWithTag("btnNext").onFirst().performClick()
-        rule.onAllNodesWithText("test2").onFirst().assertExists()
-        rule.onAllNodesWithTag("btnNext").onFirst().performClick()
-        rule.onAllNodesWithText("test3").onFirst().assertExists()
-        rule.onAllNodesWithTag("btnNext").onFirst().performClick()
-    }
 }
