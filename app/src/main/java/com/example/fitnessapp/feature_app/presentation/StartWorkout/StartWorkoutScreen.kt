636e77d45e015d0ba309f8679d1c045ff39d3e72
Fi44er <vaceslavmalugin81@gmail.com>
2025-03-03 13:58:22 +0300

    refactor

--- app/src/main/java/com/example/fitnessapp/feature_app/presentation/StartWorkout/StartWorkoutScreen.kt
+++ app/src/main/java/com/example/fitnessapp/feature_app/presentation/StartWorkout/StartWorkoutScreen.kt
@@ -9,30 +9,37 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.media3.common.util.UnstableApi
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
+import com.example.fitnessapp.feature_app.presentation.Route
+import com.example.fitnessapp.feature_app.presentation.StartWorkout.components.CustomPerformingWorkout
 import com.example.fitnessapp.feature_app.presentation.common.CustomAlertDialog
 import com.example.fitnessapp.feature_app.presentation.common.CustomGreenButton
 import com.example.fitnessapp.feature_app.presentation.common.CustomIndicator
 import com.example.fitnessapp.feature_app.presentation.common.CustomTopAppBar
-import com.example.fitnessapp.feature_app.presentation.Route
-import com.example.fitnessapp.feature_app.presentation.StartWorkout.components.CustomPerformingWorkout
 import com.example.fitnessapp.feature_app.presentation.ui.theme._F7F8F8
 import com.example.fitnessapp.feature_app.presentation.ui.theme.montserrat40012_B6B4C2
 import com.example.fitnessapp.feature_app.presentation.ui.theme.montserrat50012_A5A3B0
 import com.example.fitnessapp.feature_app.presentation.ui.theme.montserrat60016Bold_1D1617
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import org.koin.androidx.compose.koinViewModel
 
-@androidx.annotation.OptIn(UnstableApi::class)
 @Composable
 fun StartWorkoutScreen(
     navController: NavController,
@@ -40,6 +47,19 @@ fun StartWorkoutScreen(
 ) {
 
     val state = viewModel.state.value
+    val context = LocalContext.current
+    val videoUrl =
+        "https://yandex.ru/video/preview/16440480486051164973?from=tabbar&parent-reqid=1740976113805496-11373023267903828608-balancer-l7leveler-kubr-yp-sas-164-BAL&text=video+player+jetpack+compose.mp4"
+    val exoPlayer = remember(context) {
+        ExoPlayer.Builder(context).build().apply {
+            val dataSourceFactory =
+                DefaultDataSourceFactory(context, Util.getUserAgent(context, context.packageName))
+            val source = DefaultMediaSourceFactory(dataSourceFactory).createMediaSource(
+                MediaItem.fromUri(videoUrl)
+            )
+            prepare(source)
+        }
+    }
 
     if (state.exception.isNotEmpty()) {
         CustomAlertDialog(
@@ -67,13 +87,23 @@ fun StartWorkoutScreen(
         }
 
         item {
-            AsyncImage(
-                model = "https://qappxorzuldxgbbwlxvt.supabase.co/storage/v1/object/public/image//Video-Section.png",
-                contentDescription = null,
+//            AsyncImage(
+//                model = "https://qappxorzuldxgbbwlxvt.supabase.co/storage/v1/object/public/image//Video-Section.png",
+//                contentDescription = "feature video",
+//                modifier = Modifier
+//                    .fillMaxWidth(),
+//                contentScale = ContentScale.Crop
+//            )
+            AndroidView(
+                factory = { context ->
+                    PlayerView(context).apply {
+                        player = exoPlayer
+                    }
+                },
                 modifier = Modifier
-                    .fillMaxWidth(),
-                contentScale = ContentScale.Crop
-            )
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(22.dp))
+            ) {}
             Spacer(Modifier.height(20.dp))
             Text(
                 text = Route.StartWorkoutScreen.workout,
