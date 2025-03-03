c0f2f9f4f06c93f80afffaeb3c097982ee89469f
S <S>
2025-03-03 19:09:32 +0300

    refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,6 @@ plugins {
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.androidLibrary) apply false
     //alias(libs.plugins.ksp) apply false
+
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
